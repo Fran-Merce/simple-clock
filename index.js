e7722ea8a3576a92e611d48343da39e274369630
Francisco Merce <franmerce1@gmail.com>
2021-12-28 16:11:54 -0300

    finish

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const deg = 6
 const hr = document.querySelector('#hr');
 const min = document.querySelector('#mn');
 const sec = document.querySelector('#sc');
+const digitalTime = document.querySelector('#digitalTime')
+const digitalDate = document.querySelector('#date')
 
 
 
@@ -15,21 +17,15 @@ const getTime = () => {
     hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
     min.style.transform = `rotateZ(${mm}deg)`;
     sec.style.transform = `rotateZ(${ss}deg)`;
-    // console.log(hh);
     date()
     time()
 }
 setInterval(getTime, 1000)
 
-const digitalTime = document.querySelector('#digitalTime')
-const digitalDate = document.querySelector('#date')
 
 function time() {
     let day = new Date()
     digitalTime.textContent = day.toLocaleTimeString()
-
-    
-    
 }
 
 function date() {
